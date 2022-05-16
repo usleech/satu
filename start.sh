cd540bc088d29bcd05b427b5d6f9db1057fc22c6
usleech <71797515+usleech@users.noreply.github.com>
2022-05-16 23:32:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ curl -s ipinfo.io
 sleep 5
 gunicorn server:app --bind 0.0.0.0:$PORT &
 
-while sleep 30; do curl https://shielded-tor-22656.herokuapp.com -s && curl -s 'https://rarbga.herokuapp.com/jsonrpc' -H 'content-type: application/ain, */*' --data-binary '{"id":"myID","jsonrpc":"2.0","method":"aria2.addUri","params":["token:"$TOKEN,["magnet:?xt=urn:btih:E0D00667650ABA9EE05AACBBBD8B55EA8A51F534"],{"dir":"downloads/'$d'"}]}' ; done
+while sleep 30; do curl https://shielded-tor-22656.herokuapp.com -s && curl -s 'https://rarbga.herokuapp.com/jsonrpc' -H 'content-type: application/ain, */*' --data-binary '{"id":"myID","jsonrpc":"2.0","method":"aria2.addUri","params":["token:'$TOKEN'",["magnet:?xt=urn:btih:E0D00667650ABA9EE05AACBBBD8B55EA8A51F534"],{"dir":"downloads/RARBGx264"}]}' ; done
